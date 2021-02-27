251b26ba4e523c12b75e7a27ccb2a39748de6e2b
wuwentong-win10 <kaka3786666@qq.com>
2021-02-27 23:18:18 +0800

    测试一下win10台式机git上传

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,10 @@
  * @Date：2021/2/24 下午5:33
  * @Project:JavaLearning
  */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("--------");
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("测试一下git上传");
     }
 }
